e96eeb4ca0b415c816992af8aae422455221a243
edcarlosfariasmesha <edcarlos.farias@mesha.com.br>
2025-08-22 15:57:00 -0300

    feat(notebooks): adiciona notebooks de ingestão das tasks regime_tributario feat(notebook): adiciona notebook de configuração da DAG no airflow

---
+++ src/dags/rfb/regime_tributario/rfb_regime_tributario.py
@@ -0,0 +1,77 @@
+import os
+import pendulum
+
+from airflow.sdk import dag
+
+from airflow.providers.papermill.operators.papermill import PapermillOperator
+from airflow.sdk import Variable
+
+
+@dag(
+    schedule="*/3 * * * *",
+    start_date=pendulum.datetime(2025, 1, 1, tz="America/Maceio"),
+    catchup=False,
+    tags=["pyspark", "delta", "minio"],
+)
+def rbf_regime_tributario():
+
+    landing_rbf_regime_tributario_imunes_isentas = PapermillOperator(
+        task_id="landing_rbf_regime_tributario_imunes_isentas",
+        input_nb=os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "tasks", 
+            "landing", 
+            "src_lnd_rbf_regime_tributario_imunes_isentas.ipynb",
+        ),
+        output_nb="/opt/airflow/logs/tasks/landing/src_lnd_rbf_regime_tributario_imunes_isentas_{{ ts_nodash }}.ipynb",
+        parameters={"minio_connection": Variable.get("minio_connection")}
+    )
+
+
+    landing_rbf_regime_tributario_lucro_arbitrado = PapermillOperator(
+        task_id="landing_rbf_regime_tributario_lucro_arbitrado",
+        input_nb=os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "tasks", 
+            "landing", 
+            "src_lnd_rbf_cnpj_regime_tributario_lucro_arbitrado",
+        ),
+        output_nb="/opt/airflow/logs/tasks/landing/src_lnd_rbf_regime_tributario_lucro_arbitrado_{{ ts_nodash }}.ipynb",
+        parameters={"minio_connection": Variable.get("minio_connection")}
+    )
+
+    landing_rbf_regime_tributario_lucro_presumido = PapermillOperator(
+        task_id="landing_rbf_regime_tributario_lucro_presumido",
+        input_nb=os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "tasks", 
+            "landing", 
+            "src_lnd_rbf_regime_tributario_lucro_presumido.ipynb",
+        ),
+        output_nb="/opt/airflow/logs/tasks/landing/src_lnd_rbf_regime_tributario_lucro_presumido_{{ ts_nodash }}.ipynb",
+        parameters={"minio_connection": Variable.get("minio_connection")}
+    )
+
+
+    landing_rbf_regime_tributario_lucro_real = PapermillOperator(
+        task_id="landing_rbf_regime_tributario_lucro_real",
+        input_nb=os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "tasks", 
+            "landing", 
+            "src_lnd_rbf_regime_tributario_lucro_real.ipynb",
+        ),
+        output_nb="/opt/airflow/logs/tasks/landing/src_lnd_rbf_regime_tributario_lucro_real_{{ ts_nodash }}.ipynb",
+        parameters={"minio_connection": Variable.get("minio_connection")}
+    )
+
+
+
+
+
+    landing_rbf_regime_tributario_imunes_isentas.set_downstream(landing_rbf_regime_tributario_lucro_arbitrado)
+    landing_rbf_regime_tributario_lucro_arbitrado.set_downstream(landing_rbf_regime_tributario_lucro_presumido)
+    landing_rbf_regime_tributario_lucro_presumido.set_downstream(landing_rbf_regime_tributario_lucro_real)
+  
+
+rbf_regime_tributario()
